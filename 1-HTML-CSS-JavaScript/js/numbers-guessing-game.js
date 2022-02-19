38cc616580216423596d153664ac3ff3b19e2404
Michael Stokes <michael.stokes.347@gmail.com>
2022-02-19 12:16:11 -0600

    commit message

---
+++ 1-HTML-CSS-JavaScript/js/numbers-guessing-game.js
@@ -0,0 +1,41 @@
+function runGame(game) {
+
+    let guessString ='';
+    let guessNumber = 0;
+    let correct = false;
+    let numTries = 0;
+
+    const randomNumber = Math.random() * 100;
+    const randomInterger = Math.floor(randomNumber);
+    const target = randomInterger + 1;
+
+    //const target = Math.floor(Math.random() * 100) + 1;
+
+    do {
+guessString = Prompt ('I am thinking of a number in the range 1 to 100.\n\nWhat is the nmber?');
+guessNumber = +guessString;
+numTries +=1;
+correct = checkGuess(guessNumber, target);
+    } while (!correct);
+
+    alert('You got it! The number was ' +target+ '.\n\nIt took you ' +numtries + ' tries to guess correctly')
+} 
+
+function checkGuess(guessNumber, target) {
+    let correct = false;
+
+    if (isNaN(guessNumber)) {
+        alert ('You have not entered a number.\n\nPlease enter a number in the 1-100 range.');
+    } else if ((guessNumber<1) || (guessNumber>100)) {
+            alert ('Please enter an interger in the 1-100 range.');
+        }
+     else if (guessNumber>target) {
+        alert('Your number is too large!')
+    } else if (guessNumber<target) {
+        alert ('Your number is too small!')
+    } else {
+        correct=true;
+    }
+    
+    return correct;
+}
