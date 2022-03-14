282eea327297d64f95fe8457722155a6bf052887
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-13 23:58:56 -0500

    minimal viable product, ship it

--- 2-Bootstrap/nucampsite/JavaScript/nucamp-site-js.js
+++ 2-Bootstrap/nucampsite/JavaScript/nucamp-site-js.js
@@ -1,7 +1,33 @@
 "use strict";
+//
+//     $(function () {
+//     $('[data-toggle="tooltip"]').tooltip()
+// })
 
-<script>
-    $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
-</script>
+
+$(function() {
+    // Carousel Button
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+
+    //Reserve Button
+    $("#reserveBtn").click(function(){
+        $("#reserveModal").modal("show");
+    });
+
+    //Login
+    $("#loginBtn").click(function(){
+        $("#loginModal").modal("show");
+    });
+
+});
