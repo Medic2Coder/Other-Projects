a440d5218f68baa471112427d66ef605feefd1af
Michael Stokes <michael.stokes.347@gmail.com>
2022-03-22 17:49:21 -0500

    circle back

--- 2-Bootstrap/workshop5.js
+++ 2-Bootstrap/workshop5.js
@@ -31,3 +31,52 @@ if ( testStudent.name === 'Bugs Bunny' && testStudent.email === 'bugs@bunny.com'
 //     && Array.isArray(reactBootcamp.students) && reactBootcamp.students.length === 0) {
 //     console.log('TASK 2: PASS');
 // }
+
+class Bootcamp {
+    constructor(name, level, students) {
+        this.name = name;
+        this.level = level;
+        this.students = [];
+    }
+}
+reactBootcamp = new Bootcamp("React", "Advanced");
+console.log(reactBootcamp);
+if ( reactBootcamp.name === 'React' && reactBootcamp.level === 'Advanced'
+    && Array.isArray(reactBootcamp.students) && reactBootcamp.students.length === 0) {
+    console.log('TASK 2: PASS');
+}
+
+//TODO: Task 3
+
+//Add a method to the bootcamp class
+//one parameter, student two register
+
+const runTest = (bootcamp, student) => {
+    const attemptOne = bootcamp.registerStudent(student);
+    const attemptTwo = bootcamp.registerStudent(student);
+    const attemptThree = bootcamp.registerStudent(new Student("Babs Bunny"));
+    if ( attemptOne && !attemptTwo && !attemptThree) {
+        console.log("TASK 3: PASS");
+    }
+    //TODO: Task4
+//no parameters needed
+    bootcamp.registerStudent(new Student('Babs Bunny', 'babs@bunny.com'));
+    if (bootcamp.listStudents()) {
+        console.log("TASK 4: PASS 1/2");
+    }
+    bootcamp.students = [];
+    if (!bootcamp.listStudents()) {
+        console.log("TASK 4: PASS 2/2");
+    }
+
+
+    
+};
+
+//calling or invoking the function
+runTest(reactBootcamp, testStudent);
+
+
+
+//Task4
+//no parameters needed
