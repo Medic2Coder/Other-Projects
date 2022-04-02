89ead66cd4d3434436cad9f25db2ba578a06645c
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-02 11:50:33 -0500

    hearts svg to jsx

--- i-heart-react/src/App.js
+++ i-heart-react/src/App.js
@@ -1,6 +1,6 @@
 // import './AppStyleOriginal.css';
 //
-function App() {
+// function App() {
   // return (
   //   <div className="App">
   //     <header className="App-header">
@@ -19,10 +19,27 @@ function App() {
   //     </header>
   //   </div>
   // );
+// }
+import HeartLogo from './heart.svg';
+import './App.css';
+
+const message = 'cool as a cucumber';
+
+const Heart = (props) => {
+    return (
+        <div className='heart'>
+            <img className='heart-img' src={HeartLogo} alt='heart' />
+            <p className='heart-message'>{props.msg}</p>
+        </div>
+    );
 }
 
-// export default App;
+function App() {
+    return <Heart msg={message} />;
+}
+
+export default App;
 
 
 
-
+          //notional bonus
