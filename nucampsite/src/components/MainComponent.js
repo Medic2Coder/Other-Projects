853a83dfe929175f35389a58c95d7a8a56295be6
Michael Stokes <michael.stokes.347@gmail.com>
2022-05-21 17:20:10 -0500

    Initial Setup

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -1,62 +1,62 @@
-import React, { Component } from 'react';
-import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import Home from './HomeComponent';
-import Contact from './ContactComponent';
-import About from './AboutComponent';
-import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { addComment, fetchCampsites } from '../redux/ActionCreators';
-import { actions } from 'react-redux-form';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
-
-
-
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            partners: PARTNERS,
-            promotions: PROMOTIONS
-        };
-    }
-
-    const HomePage = () => {
-        return (
-            <Home
-                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
-                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
-                partner={this.state.partners.filter(partner => partner.featured)[0]}
-            />
-        );
-    };
-
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
-    render() {
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
-                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-                    <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
-                    <Redirect to='/home' />
-                </Switch>
-                <Footer />
-            </div>
-        );
-    };
-}
-
-export default Main;
+// import React, { Component } from 'react';
+// import Directory from './DirectoryComponent';
+// import CampsiteInfo from './CampsiteInfoComponent';
+// import Header from './HeaderComponent';
+// import Footer from './FooterComponent';
+// import Home from './HomeComponent';
+// import Contact from './ContactComponent';
+// import About from './AboutComponent';
+// import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { addComment, fetchCampsites } from '../redux/ActionCreators';
+// import { actions } from 'react-redux-form';
+// import { COMMENTS } from '../shared/comments';
+// import { PARTNERS } from '../shared/partners';
+// import { PROMOTIONS } from '../shared/promotions';
+//
+//
+//
+// class Main extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             campsites: CAMPSITES,
+//             comments: COMMENTS,
+//             partners: PARTNERS,
+//             promotions: PROMOTIONS
+//         };
+//     }
+//
+//     const HomePage = () => {
+//         return (
+//             <Home
+//                 campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+//                 promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+//                 partner={this.state.partners.filter(partner => partner.featured)[0]}
+//             />
+//         );
+//     };
+//
+//     onCampsiteSelect(campsiteId) {
+//         this.setState({selectedCampsite: campsiteId});
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <Header />
+//                 <Switch>
+//                     <Route path='/home' component={HomePage} />
+//                     <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+//                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+//                     <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+//                     <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
+//                     <Redirect to='/home' />
+//                 </Switch>
+//                 <Footer />
+//             </div>
+//         );
+//     };
+// }
+//
+// export default Main;
